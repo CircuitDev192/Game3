50710f898932984cb792cc8be2a39caf4e375b57
Matthew Ashmore <54996731+CircuitDev192@users.noreply.github.com>
2020-04-03 19:20:52 -0500

    Grenade Added / Throwable Weapon Framework
    Values for reload time and other parameters need major tweaking, but it works now. Will add more throwables in the future.

---
+++ Assets/Scripts/Weapons/ThrowableWeapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableWeapon : WeaponBase
+{
+    [SerializeField]
+    private GameObject fragGrenadeThrown;
+
+    public override IEnumerator Fire(Transform directionTransform)
+    {
+        yield return new WaitForSeconds(fireAnimationStartDelay);
+
+        roundsInCurrentMag--;
+        EventManager.TriggerAmmoCountChanged(roundsInCurrentMag);
+
+        //instantiate grenade
+        GameObject frag = Instantiate(fragGrenadeThrown, this.gameObject.transform);
+        frag.transform.parent = null;
+        //disable renderer
+        weaponRenderer.enabled = false;
+
+
+        frag.GetComponent<Rigidbody>().AddForce(directionTransform.forward.normalized * 2000);
+
+        yield return new WaitForSeconds(0.2f);
+        frag.GetComponent<MeshCollider>().enabled = true;
+
+        // wait one frame
+        yield return new WaitForEndOfFrame();
+
+    }
+
+    public override void ToggleFireMode()
+    {
+
+    }
+
+    protected override void OnEnable()
+    {
+        weaponRenderer.enabled = true;
+
+        EventManager.TriggerWeaponChanged(name);
+        EventManager.TriggerAmmoCountChanged(roundsInCurrentMag);
+    }
+
+    protected override void OnDisable()
+    {
+        weaponRenderer.enabled = false;
+    }
+}
