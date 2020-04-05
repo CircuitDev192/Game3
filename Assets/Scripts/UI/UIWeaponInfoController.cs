f7347d4fb46c56a7bdcd2a93d174b0a5e56d3a56
Rhett Fereday <gmfereday@gmail.com>
2020-04-05 17:33:56 -0500

    Base UI is mostly in place. Just a few minor changes needed.

---
+++ Assets/Scripts/UI/UIWeaponInfoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIWeaponInfoController : MonoBehaviour
+{
+    [SerializeField] private Text weaponName;
+    [SerializeField] private Text roundsInMag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.WeaponChanged += WeaponNameChanged;
+        EventManager.AmmoCountChanged += RoundsInMagChanged;
+    }
+
+    private void WeaponNameChanged(string weaponName)
+    {
+        this.weaponName.text = weaponName;
+    }
+
+    private void RoundsInMagChanged(int roundsInMag)
+    {
+        this.roundsInMag.text = roundsInMag.ToString("D2");
+    }
+}
