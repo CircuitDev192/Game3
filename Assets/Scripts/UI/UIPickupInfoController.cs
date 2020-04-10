d9ec1d948fbaca22717f3f96e1e90f50a3579819
Matthew Ashmore <54996731+CircuitDev192@users.noreply.github.com>
2020-04-10 16:29:27 -0500

    Weapon Pickup UI Implemented

---
+++ Assets/Scripts/UI/UIPickupInfoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIPickupInfoController : MonoBehaviour
+{
+    [SerializeField] private Text promptText;
+    [SerializeField] private Text pickupText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.PlayerCollidedWithPickup += PlayerCollidedWithPickup;
+        EventManager.PlayerLeftPickup += PlayerLeftPickup;
+    }
+
+    private void PlayerCollidedWithPickup(string weaponName)
+    {
+        promptText.gameObject.SetActive(true);
+        pickupText.gameObject.SetActive(true);
+        pickupText.text = weaponName;
+
+    }
+
+    private void PlayerLeftPickup()
+    {
+        promptText.gameObject.SetActive(false);
+        pickupText.gameObject.SetActive(false);
+    }
+
+}
